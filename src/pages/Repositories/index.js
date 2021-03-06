9c0f470c7b5fe4b3e152b8c22472c3bc215e4d57
Davi Souza <davissbf@gmail.com>
2021-03-05 22:51:53 -0300

    create-pagerepositories

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MainPage = () => <h1>Repositories</h1>;
+
+export default MainPage;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from '../pages/MainPage';
+import Repositories from '../pages/Repositories';
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route path="/repositories">
+          <Repositories />
+        </Route>
+        <Route exact path="/">
           <Main />
         </Route>
       </Switch>
