37c8d7e6f979943f81c13a2a98fe68b33c51d3eb
Davi Souza <davissbf@gmail.com>
2021-03-07 14:25:21 -0300

    create Repositories

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Profile from './Profile';
 import Filter from './Filter';
+import Repositories from './repositories';
 
 import { Container, Sidebar, Main } from './styles';
 
@@ -11,7 +12,9 @@ const RepositoriePage = () => (
       <Profile />
       <Filter />
     </Sidebar>
-    <Main>Main</Main>
+    <Main>
+      <Repositories />
+    </Main>
   </Container>
 );
 
---
+++ src/pages/RepositoriesPage/repositories/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Repositories = () => <h1>Repositories</h1>;
+
+export default Repositories;
