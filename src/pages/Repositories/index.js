49035140d9c410dacc479ce4acf87c344d69636a
Davi Souza <davissbf@gmail.com>
2021-03-07 12:16:34 -0300

    rename repositoriespage  import filter

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 import Profile from './Profile';
+import Filter from './Filter';
 
 import { Container, Sidebar, Main } from './styles';
 
-const MainPage = () => (
+const RepositoriePage = () => (
   <Container>
     <Sidebar>
       <Profile />
+      <Filter />
     </Sidebar>
     <Main>Main</Main>
   </Container>
 );
 
-export default MainPage;
+export default RepositoriePage;
