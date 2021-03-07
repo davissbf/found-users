c60b029ea1581a1bce9eed164d8ad01496559a34
Davi Souza <davissbf@gmail.com>
2021-03-07 14:19:45 -0300

    change name repositoriespage

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from '../pages/MainPage';
-import Repositories from '../pages/Repositories';
+import Repositories from '../pages/RepositoriesPage';
 
 export default function Routes() {
   return (
