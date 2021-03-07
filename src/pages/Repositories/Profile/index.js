a3cfefda7e07b75d7734b29621f2f02a85658559
Davi Souza <davissbf@gmail.com>
2021-03-07 11:57:46 -0300

    styling-repositories

--- src/pages/Repositories/Profile/index.js
+++ src/pages/Repositories/Profile/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Container, Header, Avatar, Login, Name } from './styles';
+import { MdGroup, MdWork, MdLocationCity, MdLink } from 'react-icons/md';
+import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles';
 
 const Profile = () => (
   <Container>
@@ -9,6 +10,25 @@ const Profile = () => (
       <Login>Davi</Login>
       <Name>Davissbf</Name>
     </Header>
+    <Inner>
+      <Data>
+        {' '}
+        <MdGroup size={20} />
+        30&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;10&nbsp;<i>seguindo</i>
+      </Data>
+      <Data>
+        {' '}
+        <MdWork size={20} /> Dev and Pentester
+      </Data>
+      <Data>
+        {' '}
+        <MdLocationCity size={20} /> Brasília
+      </Data>
+      <Data>
+        <MdLink size={20} />
+        <a href="https://davissbf.github.io/myportifolioreact/">myPortifolio</a>
+      </Data>
+    </Inner>
   </Container>
 );
 
