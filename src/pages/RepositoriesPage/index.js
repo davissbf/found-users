31d0c2dc97d5eab39a6ae8dc10830b0bf7a87908
Davi Souza <davissbf@gmail.com>
2021-03-08 21:31:17 -0300

    create static user

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -6,16 +6,28 @@ import Repositories from './Repositories';
 
 import { Container, Sidebar, Main } from './styles';
 
-const RepositoriePage = () => (
-  <Container>
-    <Sidebar>
-      <Profile />
-      <Filter />
-    </Sidebar>
-    <Main>
-      <Repositories />
-    </Main>
-  </Container>
-);
+const RepositoriePage = () => {
+  const user = {
+    login: 'Davi Souza',
+    avatar_url: 'https://avatars.githubusercontent.com/u/53843375?v=4',
+    followers: 0,
+    followings: 0,
+    company: null,
+    blog: 'https://davissbf.github.io/myportifolioreact/',
+    location: 'Brasília',
+  };
+
+  return (
+    <Container>
+      <Sidebar>
+        <Profile user={user} />
+        <Filter />
+      </Sidebar>
+      <Main>
+        <Repositories />
+      </Main>
+    </Container>
+  );
+};
 
 export default RepositoriePage;
