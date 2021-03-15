8a2c5fa96ad2a0865f84b16c9fe0add3a9f59997
Davi Souza <davissbf@gmail.com>
2021-03-14 23:33:43 -0300

    add lib particlesbg

---
+++ src/components/Square/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ParticlesBg from 'particles-bg';
+
+const config = {
+  num: [20, 7],
+  rps: 0.1,
+  radius: [5, 40],
+  life: [1.5, 3],
+  v: [2, 3],
+  tha: [-40, 40],
+  alpha: [0.6, 0],
+  scale: [1, 0.1],
+  position: 'center',
+  cross: 'dead',
+  random: 15,
+  type: 'cobweb',
+  g: 5,
+  onParticleUpdate: (ctx, particle) => {
+    ctx.beginPath();
+    ctx.rect(
+      particle.p.x,
+      particle.p.y,
+      particle.radius * 2,
+      particle.radius * 2
+    );
+    ctx.fillStyle = particle.color;
+    ctx.fill();
+    ctx.closePath();
+  },
+};
+
+const Square = () => (
+  <>
+    <ParticlesBg num={30} color="#233240" config={config} type="cobweb" bg />
+  </>
+);
+
+export default Square;
--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -5,11 +5,14 @@ import logoIgm from '../../assets/github-logo.svg';
 
 import { Container, Form, Input, Button } from './styles';
 
+import Square from '../../components/Square/index';
+
 const MainPage = () => {
   const [login, setLogin] = useState('');
 
   return (
     <Container>
+      <Square />
       <img src={logoIgm} alt="Logo Github" />
       <h1>API Github</h1>
       <Form>
